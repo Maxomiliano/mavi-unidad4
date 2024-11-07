704e4a399356f9589ce630b41a841749b1f129df
Maxomiliano <max.nebula1@gmail.com>
2024-11-07 00:03:35 -0300

    Atrapado

--- SMLFConfig/SMLFConfiguration/Atrapado.cpp
+++ SMLFConfig/SMLFConfiguration/Atrapado.cpp
@@ -10,15 +10,36 @@ Atrapado::Atrapado()
 	Mouse mouse;
 	Texture _ysquareTex;
 	Texture _rcircleTex;
-	Sprite* _ysquareSpr = new Sprite();
-	Sprite* _rcircleSpr = new Sprite();
+	Sprite* _currentSpr = new Sprite();
+	bool isSquare = true;
+
+	float scaleX;
+	float scaleY;
+	float heightSquare;
+	float heightCircle;
+	float widthSquare;
+	float widthCircle;
+
+	float movementPos = 5.0f;
+
 
 	_ysquareTex.loadFromFile("cuad_yellow.png");
 	_rcircleTex.loadFromFile("rcircle.png");
-	_ysquareSpr->setTexture(_ysquareTex);
-	_rcircleSpr->setTexture(_rcircleTex);
+	_currentSpr->setTexture(_ysquareTex);
+	_currentSpr->setPosition(500, 500);
 
-	RenderWindow window(VideoMode(800, 600, 32), "Splats");
+	
+	widthSquare = (float)_ysquareTex.getSize().x;
+	heightSquare = (float)_ysquareTex.getSize().y;
+	widthCircle =  (float)_rcircleTex.getSize().x;
+	heightCircle = (float)_rcircleTex.getSize().y;
+
+	scaleX = widthCircle / widthSquare;
+	scaleY = heightCircle / heightSquare;
+
+	_currentSpr->setScale(scaleX, scaleY);
+
+	RenderWindow window(VideoMode(800, 800, 32), "Splats");
 
 	while (window.isOpen())
 	{
@@ -29,7 +50,39 @@ Atrapado::Atrapado()
 			case Event::Closed:
 				window.close();
 				break;
+			case Event::KeyPressed:
+				if (Keyboard::isKeyPressed(Keyboard::W))
+				{
+					_currentSpr->move(0, -movementPos);
+				}
+				if (Keyboard::isKeyPressed(Keyboard::A))
+				{
+					_currentSpr->move(-movementPos, 0);
+				}
+				if (Keyboard::isKeyPressed(Keyboard::S))
+				{
+					_currentSpr->move(0, movementPos);
+				}
+				if (Keyboard::isKeyPressed(Keyboard::D))
+				{
+					_currentSpr->move(movementPos, 0);
+				}
+				if (Keyboard::isKeyPressed(Keyboard::Space) && isSquare == true)
+				{
+					_currentSpr->setTexture(_rcircleTex);
+					_currentSpr->setScale(1, 1);
+					isSquare = false;
+				}
+				else if (Keyboard::isKeyPressed(Keyboard::Space) && isSquare == false)
+				{
+					_currentSpr->setTexture(_ysquareTex);
+					_currentSpr->setScale(scaleX, scaleY);
+					isSquare = true;
+				}
 			}
 		}
+		window.clear();
+		window.draw(*_currentSpr);
+		window.display();
 	}
 }
